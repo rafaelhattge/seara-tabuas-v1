80113da6e705ba83353c40d035f4347b6da1eca3
Rafael Ivan Hattge <rafaelhattge@gmail.com>
2020-08-10 05:44:00 -0300

    Updated meta tags

--- js/script.js
+++ js/script.js
@@ -7,6 +7,24 @@
 var homePage = gsap.timeline({ paused: true });
 var productsPage = gsap.timeline({ paused: true });
 var boardsPage = gsap.timeline({ paused: true });
+var selScrollable = '.scrollable';
+// Uses document because document will be topmost level in bubbling
+$(document).on('touchmove',function(e){
+  e.preventDefault();
+});
+// Uses body because jQuery on events are called off of the element they are
+// added to, so bubbling would not work if we used document instead.
+$('body').on('touchstart', selScrollable, function(e) {
+  if (e.currentTarget.scrollTop === 0) {
+    e.currentTarget.scrollTop = 1;
+  } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+    e.currentTarget.scrollTop -= 1;
+  }
+});
+// Stops preventDefault from being called on document if it sees a scrollable div
+$('body').on('touchmove', selScrollable, function(e) {
+  e.stopPropagation();
+});
 
 $(document).ready(function () {
 
